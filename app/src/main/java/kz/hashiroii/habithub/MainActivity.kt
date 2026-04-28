9afb674d19febad0e04e4319e7afcf49347b3edc
magzhan <mubarakulymagzhan@gmail.com>
2026-04-29 00:14:21 +0500

    feat(app): wire MainActivity to NavHost with home as start destination

--- app/src/main/java/kz/hashiroii/habithub/MainActivity.kt
+++ app/src/main/java/kz/hashiroii/habithub/MainActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kz.hashiroii.core.designsystem.theme.HabitHubTheme
+import kz.hashiroii.feature.home.navigation.HomeRoute
+import kz.hashiroii.feature.home.navigation.homeScreen
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -21,29 +19,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HabitHubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HabitHubNavHost()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HabitHubTheme {
-        Greeting("Android")
+private fun HabitHubNavHost() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoute,
+    ) {
+        homeScreen()
     }
 }
